974a4b9a52240b60a3a224ac822151793df738a0
Mateusz Kolodziejski <mateusz.kolodziejski@misys.com>
2017-02-28 15:28:13 +0100

    Add JettyServer.

---
+++ server/src/main/java/com/mattsource/discordbucket/server/JettyServer.java
@@ -0,0 +1,32 @@
+package com.mattsource.discordbucket.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+class JettyServer {
+
+    private final Server server;
+
+    JettyServer() {
+        this.server = new Server(1717);
+
+        ResourceConfig config = new ResourceConfig();
+        config.packages("com.mattsource.discordbucket.rest");
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+        ServletContextHandler context = new ServletContextHandler(server, "/*");
+        context.addServlet(servlet, "/*");
+    }
+
+    void serve() throws Exception {
+        try {
+            server.start();
+            server.join();
+        } finally {
+            server.destroy();
+        }
+    }
+}
